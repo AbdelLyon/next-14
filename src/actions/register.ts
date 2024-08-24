faffb89a17a53520de42705dda22bb1e663b338e
AbdelLyon <abdelmajidahamiane@gmail.com>
2024-08-24 04:59:35 +0200

    email activation

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -2,7 +2,7 @@
 
 import { createUser } from "@/db/user";
 import { signJwt } from "@/auth/jwt";
-import { compileActivationTemplate, sendMail } from "@/mail/mail";
+import { sendMail } from "@/emails/mail";
 import { RegisterPayload } from "@/validators/registerSchema";
 import { genSaltSync, hash } from "bcryptjs";
 
@@ -16,21 +16,25 @@ export async function registerAction(
     // Création de l'utilisateur dans la base de données
     const newUser = await createUser(user, hashedPassword);
 
+    console.log(newUser);
+
     if (!newUser) throw new Error("Échec de la création de l'utilisateur");
 
     // Génération du JWT pour l'utilisateur
     const jwtUserId = signJwt({ id: newUser.id });
     const activationUrl = `${process.env.NEXTAUTH_URL}/auth/activation/${jwtUserId}`;
 
-    // Compilation du modèle de l'email d'activation
-    const body = compileActivationTemplate(user.firstname, activationUrl);
-
     // Envoi de l'email d'activation
-    await sendMail({ to: user.email, subject: "Activez votre compte", body });
+    await sendMail({
+      to: [user.email],
+      name: user.firstname,
+      subject: "Activez votre compte",
+      url: activationUrl,
+    });
 
     return newUser;
   } catch (error) {
-    console.error("Erreur lors de l'inscription", error);
+    console.error("Erreur lors de l'inscription");
     throw new Error("L'inscription a échoué");
   }
 }
